9c7f59c964c6304609ade6d00540f42e0bde82c8
Leonid <halaax@yandex.ru>
2017-08-15 16:33:48 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,15 +1,38 @@
 // useful php functions
-
-- implode(', ', $row) /* 1 arg - delimeter, 2 - array.   It makes a string by combining all the values in an array,
+------------------------------------------------------------------------------------------------------------------------------------------
+implode(', ', $row) /* 1 arg - delimeter, 2 - array.   It makes a string by combining all the values in an array,
 putting a string delimiter between each value */
-
-- /* Use the query() method to retrieve information from the database. Pass it an SQL
+------------------------------------------------------------------------------------------------------------------------------------------
+/* Use the query() method to retrieve information from the database. Pass it an SQL
 query for the database. It returns a PDOStatement object that provides access to the
 retrieved rows. Each time you call the fetch() method of this object, you get the next
 row returned from the query. When there are no more rows left, fetch() returns a
-value that evaluates to false */
+value that evaluates to false. To return all use fetchAll(). */
 
 $q = $db->query('SELECT dish_name, price FROM dishes');
 while ($row = $q->fetch()) {
     print "$row[dish_name], $row[price] \n";
 }
+
+------------------------------------------------------------------------------------------------------------------------------------------
+
+Use the query() method to retrieve information from the database. Pass it an SQL
+query for the database. It returns a PDOStatement object that provides access to the
+retrieved rows. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
